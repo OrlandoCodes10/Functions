cf2c395d60b1798424317ba84c169702eb0c7f4c
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-02-18 17:35:45 -0500

    Lexical scope practice

---
+++ Scope/app.js
@@ -0,0 +1,44 @@
+// let totaleEggs = 0;
+// function collectEggs() {
+//     totaleEggs = 6;
+// }
+// console.log(totalEggs);
+
+// //Function Scope
+// function helpMe() {
+//     let msg = "I'm on fire!";
+//     msg; //"I'm on fire";
+// }
+// * msg is scoped to the helpMr function
+// msg; //NOT DEFINED!
+
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     let bird = 'Great Blue Heron';
+// }
+
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = 'HIII!'
+// }
+
+// console.log(radius);
+
+// for (let i = 0; i < 5; i++) {
+//     let msg = "DFDSFDF";
+//     console.log(msg)
+// }
+// console.log(msg)
+// console.log(i)
+
+// function bankRobbery() {
+//     const heroes = ['Spiderman', 'Wolverine', 'Black Panther']
+//     function cryForHelp() {
+//         for (let hero of heroes) {
+//             console.log(`PLEASE HELP US, ${hero.toUpperCase()}`)
+//         }
+//     }
+//     cryForHelp();
+// }
+
